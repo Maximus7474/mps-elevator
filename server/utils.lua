ab749f6edcab6a8d3be00101821d43d225dd6e48
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-10 13:19:30 +0200

    feat(server/utils): helper function to sanitize groups & items restrictions

---
+++ server/utils.lua
@@ -0,0 +1,51 @@
+---@param groups? string | string[] | table<string, number>
+---@return false | table<string, number>
+function SanitizeGroups(groups)
+    if (not groups) then return false end
+
+    local sanitizedGroups = {}
+
+    if (type(groups) == "string") then
+        sanitizedGroups[groups] = 0
+
+    elseif (type(groups) == "table") then
+
+        for key, value in pairs(groups) do
+
+            if (type(key) == "number") then
+                sanitizedGroups[key] = 0
+
+            elseif (type(key) == "string" and type(value) == "number") then
+                sanitizedGroups[key] = value
+            end
+        end
+    end
+
+    return next(sanitizedGroups) and sanitizedGroups or false
+end
+
+---@param items? string | string[] | table<string, table<string, any>>
+---@return false | table<string, true | table<string, any>>
+function SanitizeItems(items)
+    if (not items) then return false end
+
+    local sanitizedItems = {}
+
+    if (type(items) == "string") then
+        sanitizedItems[items] = true
+
+    elseif (type(items) == "table") then
+
+        for key, value in pairs(items) do
+
+            if (type(key) == "number") then
+                sanitizedItems[key] = true
+
+            elseif (type(key) == "string" and type(value) ~= "table") then
+                sanitizedItems[key] = value
+            end
+        end
+    end
+
+    return next(sanitizedItems) and sanitizedItems or false
+end
