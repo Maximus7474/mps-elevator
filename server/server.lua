b3f367614463e7e960d231bae0123413e90a777e
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-10 16:20:15 +0200

    feat(server): update player targets func & export to refresh target list for the client

--- server/server.lua
+++ server/server.lua
@@ -1,5 +1,25 @@
 if not CanResourceBeUsed(true) then return end
 
+---updates the elevators targetable by a client
+---@param source any
+local function updatePlayerTargets(source)
+    if (not tonumber(source)) then return end
+
+    local bucket = GetPlayerRoutingBucket(source)
+    local targets = {}
+
+    for _, Elevator in pairs(Elevator.elevators) do
+        local data = Elevator:getFloorPositions(source, bucket)
+
+        if (data) then
+            table.insert(targets, data)
+        end
+    end
+
+    TriggerLatentClientEvent('elevator:updateelevators', source, -1, targets)
+end
+exports('UpdatePlayerTargets', updatePlayerTargets)
+
 if Config.VersionCheck then
     lib.versionCheck('Maximus7474/mps-elevator')
 end
