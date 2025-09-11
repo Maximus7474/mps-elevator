efb3280944fc9042cbb332516e5132d5a618a14a
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-11 12:19:35 +0200

    feat(web/utils): formatFloorIcon helper

--- web/src/utils/misc.ts
+++ web/src/utils/misc.ts
@@ -1,6 +1,11 @@
 // Will return whether the current environment is in a regular browser
+
+import { FloorData } from "../types";
+
 // and not CEF
 export const isEnvBrowser = (): boolean => !(window as any).invokeNative;
 
 // Basic no operation function
 export const noop = () => {};
+
+export const formatFloorIcon = (floor: FloorData) => floor.icon ?? `${floor.id}`.padStart(2, "0");
