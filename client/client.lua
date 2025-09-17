029c4e24ef5187c61423bc32c34bdb78e94905b7
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-17 19:06:21 +0200

    refactor(client): work on new behaviour between sv <-> cl <-> nui

--- client/client.lua
+++ client/client.lua
@@ -1,40 +1,33 @@
 if not CanResourceBeUsed(true) then return end
 
-if Config.Debug then
-    RegisterCommand('show-nui', function()
-        NUI.ToggleNui(true)
-        State.UIOpen = true
-        DebugPrint('Show NUI frame')
-    end)
-end
+local NUI = require 'client.modules.nui'
 
-RegisterNUICallback('hideFrame', function(_, cb)
-    State.UIOpen = false
-    NUI.ToggleNui(false)
-    currentElevator = nil
-    DebugPrint('Hide NUI frame')
-    cb({})
+RegisterNetEvent('elevator:updateelevators', function (elevators --[[ @as {name: string; id: string; floors: vector4[]} ]])
+    for i = 1, #elevators, 1 do
+        ClElevator:new(elevators[i])
+    end
 end)
 
-RegisterNUICallback('setNewFloor', function(data, cb)
+RegisterNUICallback('hideFrame', function(_, cb)
+    NUI.ToggleNui(false)
+    DebugPrint('Hide NUI frame')
+    cb({})
 
-    if isMoving then DebugPrint("Player is already moving, cancelling") return cb(nil) end
+    TriggerServerEvent('elevator:internal:closedinterface')
+end)
 
-    isMoving = true
-    DebugPrint('Data received from NUI', json.encode(data))
+RegisterNUICallback('SetNewFloor', function(data, cb)
+    local floorIndex = data.floorIndex
 
-    local success = Citizen.Await(TP.GoToNewFloor(currentElevator, data.floorIndex))
+    cb({})
 
-    cb(success)
+    local response = lib.callback.await('elevator:internal:setnewfloor', false, floorIndex) --[[ @as { success: boolean; restricted: boolean; floors: ElevatorFloor[] } ]]
 
-    if Config.Options.CloseUI then
-        SetTimeout(success and 250 or 500, function ()
-            isMoving = false
-            NUI.ToggleNui(false)
-            State.UIOpen = false
-        end)
-    else
-        isMoving = false
-        State.UIOpen = false
-    end
+    lib.print.info('received response from "elevator:internal:setnewfloor"')
+
+    NUI.SendMessage('SetElevatorData', {
+        access = response.success and 'authorised' or 'denied',
+        restricted = response.restricted,
+        floors = response.floors,
+    });
 end)
