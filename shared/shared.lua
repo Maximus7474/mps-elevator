bae1d311a4950827863355fa2d9e038afccb5529
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-12 15:06:49 +0100

    tweak(shared): functions

--- shared/shared.lua
+++ shared/shared.lua
@@ -7,9 +7,11 @@ function DebugPrint(...)
             newArgs[k] = json.encode(
                 v,
                 {
-                    indent=4, sort_keys=true
+                    indent=true, sort_keys=true
                 }
             )
+        elseif type(v) == 'function' then
+            newArgs[k] = 'function'
         elseif type(v) ~= "string" then
             newArgs[k] = json.encode(v) or 'nil'
         else
@@ -34,7 +36,7 @@ end
 
 function CanResourceBeUsed(print_err)
     if not LoadResourceFile(GetCurrentResourceName(), 'web/build/index.html') then
-        if print_err then lib.print.error('The resource cannot be used, the UI is not built.\n   Please download a release version or build the UI: ^4https://github.com/Maximus7474/mps-elevator/releases^7') end
+        if print_err then error('The resource cannot be used, the UI is not built.\n   Please download a release version or build the UI: ^4https://github.com/Maximus7474/mps-elevator/releases^7') end
         return false
     end
     return true
